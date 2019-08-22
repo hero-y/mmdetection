a15eef5deabf2609ec03ac5867586cfa5f635737
hero-y <502750002@qq.com>
2019-08-22 09:52:37 +0800

    在exp文件中添加cv2和切片操作

--- tools/exp.py
+++ tools/exp.py
@@ -3,6 +3,9 @@ import numpy as np
 import cv2
 from collections import defaultdict
 import math
+import matplotlib.pyplot as plt
+import time
+from random import  choice
 
 def exp_dim():
     m = torch.randn(2, 1, 4)
@@ -18,6 +21,13 @@ def exp_dim():
     h = m.squeeze() #squeeze里面有参数，就代表只看该维是否是1
     i = m.unsqueeze(2)
     val,index = o.max(dim=1) #和topk一样显示val，再是Index
+    sort_val, indices = torch.sort(o, dim=1, descending = True) #dim=1，代表列发生变化
+
+    na = np.arange(12).reshape(3,4)
+    nb = np.arange(12).reshape(3,4)
+    print(np.vstack((na,nb)).shape) #(6,4)((na,nb))里面要打括号，因为这样子就相当于是一个参数的位置了
+    print(np.hstack((na,nb)).shape) #(3,8)
+    print(np.concatenate((na,nb),axis=1).shape) #(3,8)
     print('a', a)
     print('b', b)
     print('n', n)
@@ -28,7 +38,9 @@ def exp_dim():
     print('g', g.shape)
     print('h', h.shape)
     print('i', i.shape)
+    print('o',o)
     print('val',val,'index',index)
+    print('sort_val',sort_val,'\n','indices',indices)
 
 def print_indx():
     print('loss[{0}]'.format(1))
@@ -82,10 +94,38 @@ def coco_exp():
     print(a[...,None].shape)  #把前面的:都省去，None在最后一维
 
 
-def cv2_exp():
+def cv2_exp(): 
+    """
+    在图像上画框，并在左上角涂满颜色，并在中间写上类别
+    注意：更改了mmcv中的Image.py文件，使得test-show的时候也有这个效果，该文件可以通过
+    mmdet/models/detectors/base.py最下面的mmcv.imshow_det_bboxes找到
+    在if len(bbox) > 4:
+            label_text += '|{:.02f}'.format(bbox[-1])
+    和cv2.putText中间添加了如下四行
+    size = cv2.getTextSize(label_text,cv2.FONT_HERSHEY_COMPLEX,font_scale,1)
+    text_width = size[0][0]
+    text_height = size[0][1]
+    cv2.rectangle(
+        img, (bbox_int[0],bbox_int[1]-text_height-2),(bbox_int[0]+text_width,bbox_int[1]), bbox_color, thickness=-1)
+    """
     img = cv2.imread('../1.jpg')
-    img2 = np.flip(img,axis=2)
-    cv2.imshow('image',img2)
+    img2 = np.flip(img,axis=2) #随机翻转
+    bbox = np.array((30,50,200,300))
+    bbox_int = bbox.astype(np.int32)
+    left_top = (bbox_int[0],bbox_int[1])
+    right_bottom = (bbox_int[2],bbox_int[3])
+    cv2.rectangle(
+        img, left_top, right_bottom, (0, 0, 255), thickness=2)
+    label_text = 'horse'
+    label_text += '|{:.02f}'.format(0.7)
+    size = cv2.getTextSize(label_text,cv2.FONT_HERSHEY_COMPLEX,0.5,1)
+    text_width = size[0][0]
+    text_height = size[0][1]
+    cv2.rectangle(
+        img, (bbox_int[0],bbox_int[1]-text_height-2),(bbox_int[0]+text_width,bbox_int[1]), (0, 0, 255), thickness=-1)
+    cv2.putText(img, label_text, (bbox_int[0], bbox_int[1] - 2),
+                cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255))
+    cv2.imshow('image',img)
     cv2.waitKey (0)  
     cv2.destroyAllWindows() 
 
@@ -109,8 +149,31 @@ def cuda_exp():
     c = b.cuda() #如果在定义b的时候就.cuda()那么b就在cuda上，而现在b是在cpu上，c是在cuda上
     print(a*c)
 
+def index_exp():
+    m = [0,2]
+    rois = torch.randn(3,4)
+    index = rois.new_zeros((3,1))
+    index[2,:] = 1
+    rois = torch.cat([index,rois],dim=1)
+    print("rois原",rois)
+    #rois[:0] == 0,通过>,<,==等生成一个长度和原长度一致的0,1序列，在多维的时候常用来判断某一列的值，就使用[:,i]的方法
+    inds = torch.nonzero(rois[:,0] == 0).squeeze() #torch.nonzero把非零位置的索引值取出来，inds是个tensor的list
+    #rois[:0] == 0 是tensor([1,1,0]),inds是tensor([0,1])
+    b1 = rois[inds,1:]
+    print("rois经过一个tensor的List作为索引值之后",b1)
+    b2 = rois[m,1:] #对于一个tensor可以把一个List作为索引值来切片，该list可以是tensor，也可以不是
+    print("rois经过一个普通的List作为索引值之后",b2)
+    inds2 = torch.ones(len(rois),dtype = torch.uint8)
+    m = torch.tensor([0,1])
+    inds2[:len(m)] = m 
+    b3 = rois[inds2,1:] #也就是说用切片的方法对一个tensor取出一部分，可以是把对应的索引的位置带入，也可以是把一串[0,1]带入，这串值的大小和总索引的长度一样
+    print("rois经过一个和他的长度一样有0,1组成的list作为索引值之后",b3)
+
 if __name__ == '__main__':
+
+
     
+
 
 
 
