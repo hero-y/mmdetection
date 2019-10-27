89030249567a80b9c3653120fc3751fc9eaeb62e
hero-y <502750002@qq.com>
2019-10-27 21:25:06 +0800

    在mmdet/apis/inference中添加新功能:无论输入多大的图片都按(1333,800)显示，添加crop_object函数，可以把框选出的物体裁剪下来，并保存

--- tools/test_image_video.py
+++ tools/test_image_video.py
@@ -1,6 +1,7 @@
 from mmdet.apis import init_detector, inference_detector, show_result, show_result_pyplot
 import mmcv
 import argparse
+import time
 
 def parse_args():
     parser = argparse.ArgumentParser(description='MMDet test detector')
@@ -13,7 +14,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-    model = init_detector(args.config, args.checkpoint, device='cuda:0')
+    model = init_detector(args.config, args.checkpoint, device='cuda:0') 
     if args.img is not None:
         result = inference_detector(model, args.img)
         show_result(args.img, result, model.CLASSES, score_thr=0.5)
